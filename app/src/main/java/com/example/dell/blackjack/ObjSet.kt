278a74b8df27cf3f11ac8653b978e373f178e411
rinp <rinp.mail.mail@gmail.com>
2018-06-08 22:15:23 +0900

    表示に関するものを別ファイルへ移動

--- app/src/main/java/com/example/dell/blackjack/ObjSet.kt
+++ app/src/main/java/com/example/dell/blackjack/ObjSet.kt
@@ -11,15 +11,6 @@ const val ACEHIGH: Int = 11 //ACEを高い値で数える
 const val ACELOW: Int = 1 //ACEを低い値で数える
 const val JQK: Int = 10 //絵札の値
 const val BLACKJACK: Int = 21 //BLACKJACKの値
-////レイアウト
-const val CARDF = "#f5deb3"//カード表の色
-const val CARDB = "#d2b48c"//カード裏の色
-const val CARDSEL = "#deb887" //カード選択枠の色
-const val CARDW: Int = 45 //cardの横幅
-const val CARDH: Int = 60 //cardの縦幅
-//汎用キー
-const val PLAYER: String = "player" //操作側の名称
-const val DEALER: String = "dealer" //メインNPCの名称
 ////プリファレンス関連
 val PREFILENAME = "ownChip" //プリファレンスファイル名
 val PLAYERMONEY = "chip" //操作側の所持金
---
+++ app/src/main/java/com/example/dell/blackjack/layout.kt
@@ -0,0 +1,12 @@
+package com.example.dell.blackjack
+
+////レイアウト
+const val CARDF = "#f5deb3"//カード表の色
+const val CARDB = "#d2b48c"//カード裏の色
+const val CARDSEL = "#deb887" //カード選択枠の色
+const val CARDW: Int = 45 //cardの横幅
+const val CARDH: Int = 60 //cardの縦幅
+
+//汎用キー
+const val PLAYER: String = "player" //操作側の名称
+const val DEALER: String = "dealer" //メインNPCの名称
