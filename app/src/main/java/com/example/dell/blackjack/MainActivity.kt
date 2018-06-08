117f98a9cb248777cde0a8e449a9f7305941db21
rinp <rinp.mail.mail@gmail.com>
2018-06-09 00:01:20 +0900

    import整理

--- app/src/main/java/com/example/dell/blackjack/MainActivity.kt
+++ app/src/main/java/com/example/dell/blackjack/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.dell.blackjack
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 import com.google.android.gms.ads.AdListener
--- app/src/main/java/com/example/dell/blackjack/RuleActivity.kt
+++ app/src/main/java/com/example/dell/blackjack/RuleActivity.kt
@@ -1,8 +1,7 @@
 package com.example.dell.blackjack
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_rule.*
 
 class RuleActivity : AppCompatActivity() {
